80af90842762f45189439f585f1617a64524942f
Daniel Adler <daniel.adler@yabb.io>
2022-11-07 00:03:12 +0100

    More build error fixes

--- src/App.js
+++ src/App.js
@@ -8,10 +8,6 @@ import React from 'react';
 import {BrowserRouter, Routes, Route } from "react-router-dom";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     return (
       <div className="App">
